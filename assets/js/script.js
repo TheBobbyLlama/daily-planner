ad313c888610a1f09b8b653464b21e17d38d6342
Bob Feuerstein <thebobbyllama@yahoo.com>
2020-06-28 16:17:49 -0500

    Task saving logic.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,36 @@
-// schedule starts a 9AM and ends at 5PM - 9 items?
-var today = moment(9, "HH");
+// schedule starts a 9AM and ends at 5PM - 9 items.
 var schedule;
 
-var taskStates = [ "past", "present", "future" ];
+var timeStates = [ "past", "present", "future" ];
 
 var saveSchedule = function() {
-	localStorage.setItem("plannerDate", today.format("MM/DD/YYYY"));
+	localStorage.setItem("plannerDate", moment().format("MM/DD/YYYY"));
 	localStorage.setItem("plannerTasks", JSON.stringify(schedule));
 }
 
 var loadSchedule = function() {
 	var scheduleBlock = $("#scheduleBlock");
-	var plannerDate = moment(localStorage.getItem("plannerDate"));
-	var curTime = moment(today);
+	var plannerDate = moment(localStorage.getItem("plannerDate"), "MM/DD/YYYY");
+	var curTime = moment(9, "HH");
 
-	if (plannerDate.isSame(today, "day"))
-		schedule = JSON.parse(localStorage.getItem("plannerTasks")) || [];
-	else
+	// Don't keep our shedule if it's old.
+	if (plannerDate.isBefore(curTime, "day")) {
 		schedule = [];
+		saveSchedule(); // Also force localStorage to reset.
+	}
+	else
+		schedule = JSON.parse(localStorage.getItem("plannerTasks")) || [];
+		
 
 	for (var i = 0; i < 9; i++) {
 		var rowEl = $("<div>").addClass("row");
-		var timeEl = $("<div>").addClass("col-lg-1 hour").attr("data-task-time", curTime.format("hA")).text(curTime.format("hA"));
-		var descEl = $("<div>").addClass("col-11 col-lg-10").text(schedule[i]);
-		var saveEl = $("<div>").addClass("col-1 saveBtn").text("TODO");
+		var timeEl = $("<div>").addClass("col-lg-1 p-3 text-right hour").attr("data-slot-time", curTime.format("hA")).text(curTime.format("hA"));
+		var descEl = $("<div>").addClass("col-11 col-lg-10 p-0").attr("data-slot-id", i);
+		var taskEl = $("<p>").addClass("m-3 h-50").text(schedule[i]);
+		var saveEl = $("<div>").addClass("col-1 saveBtn")
+					.append("<span>").addClass("oi oi-box"); // TODO - Align the icon in the middle of the button somehow!
 
+		descEl.append(taskEl);
 		rowEl.append(timeEl).append(descEl).append(saveEl);
 		scheduleBlock.append(rowEl);
 
@@ -36,7 +42,7 @@ var loadSchedule = function() {
 
 var setTimeFormatting = function() {
 	var curState = 0;
-	var curTime = moment(today);
+	var curTime = moment(9, "HH");
 	var timeNow = moment();
 
 	// today starts at hour 9, keep going through
@@ -48,15 +54,67 @@ var setTimeFormatting = function() {
 		else
 			curState = 1;
 		
-		$("div[data-task-time = '" + curTime.format("hA") + "'] + div")
-			.removeClass((taskStates[curState] + 1) % taskStates.length)
-			.removeClass((taskStates[curState] + 2) % taskStates.length)
-			.addClass(taskStates[curState]);
+		$("div[data-slot-time = '" + curTime.format("hA") + "'] + div")
+			.removeClass((timeStates[curState] + 1) % timeStates.length)
+			.removeClass((timeStates[curState] + 2) % timeStates.length)
+			.addClass(timeStates[curState]);
 
 		curTime.add(1, "h");
 	}
 }
 
-$("#currentDay").text(today.format("dddd, MMMM Do"));
+// Clicked on a time slot.
+$("#scheduleBlock").on("click", "p", function() {
+	var text = $(this)
+    	.text()
+		.trim();
+
+	$(this).children().addClass("d-none");
+  
+	var textInput = $("<textarea>")
+    	.addClass("w-100 h-100")
+    	.val(text);
+  
+	$(this).replaceWith(textInput);
+	textInput.trigger("focus");
+});
+
+// Stopped editing a time slot.
+$("#scheduleBlock").on("blur", "textarea", function(event) {
+	var dataId = parseInt($(this).parent().attr("data-slot-id"));
+
+	// Short timer before clearing  our data, so the save button can grab it if needed.
+	setTimeout(stopEditing, 100, dataId);
+});
+
+var stopEditing = function(dataId) {
+	if (dataId !== NaN) {
+		var curTextArea = $("div[data-slot-id='" + dataId + "'] textarea");
+
+		if (curTextArea) {
+			console.log("Clearing item " + dataId);
+			var taskEl = $("<p>").addClass("m-3 h-50").text(schedule[dataId]);
+			(curTextArea).replaceWith(taskEl);
+		}
+	}
+}
+
+// Clicked on a time slot's save button.
+$("#scheduleBlock").on("click", ".saveBtn", function(event) {
+	var descDiv = $(this).prev();
+	var dataId = parseInt(descDiv.attr("data-slot-id"));
+
+	if (dataId !== NaN) {
+		var textArea = descDiv.children("textarea");
+
+		if (textArea) {
+			schedule[dataId] = textArea.val();
+			stopEditing(dataId);
+			saveSchedule();
+		}
+	}
+});
+
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
 loadSchedule();
 setInterval(setTimeFormatting, 300000); // Update time formatting every 5 minutes.
